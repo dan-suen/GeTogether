5f9862d41ea7890209177ddab8763163db37aec2
Donny Zhao <donny.zh@hotmail.com>
2022-10-02 19:17:36 -0400

    Created top-nav component

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import Button from 'react-bootstrap/Button';
+import NavBar from  './components/top_nav/top_nav'
 import axios from 'axios';
 //import './App.css';
 
@@ -29,6 +30,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar></NavBar>
         <h1>{ this.state.message }</h1>
         <Button variant="danger" onClick={this.fetchData}>
           Fetch Data
