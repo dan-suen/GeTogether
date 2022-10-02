91fe229b6404ad274416755037cfbb5ab6894bd0
Eds-Dbug <edsDbug@gmail.com>
2022-10-02 23:00:08 +0000

    add variables

---
+++ react-front-end/src/components/main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/main.scss'
+
+const Main = () => {
+  return (
+    <section className='page'>
+      <section className=''></section>
+      <section className='all_events'></section>
+    </section>
+  );
+};
+
+export default Main;
