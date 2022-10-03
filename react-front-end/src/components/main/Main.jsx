a8d065bcf4bc13a0f8cdc50d39e72fb954278bc9
Eds-Dbug <edsDbug@gmail.com>
2022-10-03 22:04:22 +0000

    create create button

--- react-front-end/src/components/main/Main.jsx
+++ react-front-end/src/components/main/Main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './main.scss'
 import Intro from './Intro';
 import Search from './Search';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 
 const Main = () => {
@@ -18,7 +20,11 @@ const Main = () => {
       </section>
 
       <section className='page__events-list'>
-        <h5 className='page__events-list__title'>Your Events</h5>
+        <div className='page__events-list__header'>
+          <h5>Your Events</h5>
+          <button className=''><FontAwesomeIcon icon={faPlus} /></button>
+        </div>
+        
         <ul class="list-group list-group-flush">
           <li class="list-group-item">An item</li>
           <li class="list-group-item">A second item</li>
