46780f94c3018e55cfceb088cc32c0aff583dca0
Donny Zhao <donny.zh@hotmail.com>
2022-10-03 11:50:10 -0400

    implement top_nav and footer draft

--- react-front-end/src/components/top_nav/top_nav.js
+++ react-front-end/src/components/top_nav/top_nav.js
@@ -2,29 +2,25 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Login from './Login'
 import "./top_nav.scss";
 
 export default function TopNav() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar expand="lg" sticky="top">
       <Container>
         <Navbar.Brand href="#home">Getogether</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav>
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+          </Nav>
+          <Nav>
+            <NavDropdown title="Login" id="basic-nav-dropdown">
+              <Login />
             </NavDropdown>
+            <Nav.Link href="#register">Sign up!+</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
