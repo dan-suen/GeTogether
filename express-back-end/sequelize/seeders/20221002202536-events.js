0e7702360d9a6a9b2168e60f0bb1a25568e5e868
Daniel Suen <danielsuen2007@yahoo.ca>
2022-10-03 11:43:42 -0400

    added multiple locations to event seed

--- express-back-end/sequelize/seeders/20221002202536-events.js
+++ express-back-end/sequelize/seeders/20221002202536-events.js
@@ -24,13 +24,21 @@ module.exports = {
     */
      const users = await sequelize.query(`SELECT MAX(id) FROM "Users"`, { type: QueryTypes.SELECT });
      let seeds = [];
+     let locations = [
+      "662 King St W #101, Toronto, ON M5V 1M7",
+      "250 Av. Clarke #1010, Westmount, QC H3Z 2E5",
+      "630 8 Ave SW #600, Calgary, AB T2P 1G6",
+      "401 W Georgia St #600, Vancouver, BC V6B 5A1",
+      "66 Slater St. #311, Ottawa, ON K1P 5K8",
+      "2031 Store St, Victoria, BC V8T 5L9"
+     ]
      for (let i = 0; i < 10; i ++) {
        const spots = Math.floor(Math.random()*(99))+1;
        const remain = Math.floor(Math.random()*(spots))+1;
        seeds.push({
          host_id:  Math.floor(Math.random()*(users[0].max-1))+1,
          event_time: faker.date.future(1) ,
-         location:"662 King St W #101, Toronto, ON M5V 1M7",
+         location:locations[i%5],
          price: Math.floor(Math.random()*(1000)),
          spots: spots,
          remaining_spots: remain,
