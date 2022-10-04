df8767b0766f9dc5723d17ea04a26afbb0565967
Daniel Suen <danielsuen2007@yahoo.ca>
2022-10-03 23:51:55 -0400

    added profile page layout

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import axios from 'axios';
-import Main from './components/main/Main';
+import Profile from './components/profile/profile';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Main></Main>     
+        <Profile/>   
       </div>
     );
   }
---
+++ react-front-end/src/components/profile/profile.js
@@ -0,0 +1,47 @@
+import "./profile.scss";
+import Event from '../main_logged/event.js';
+
+export default function Profile(props) {
+  return (
+    <>
+      <div className = "user-profile">
+        <h1>USERNAME HERE</h1>
+        <div className = "user-info">
+          <img src="https://loremflickr.com/cache/resized/65535_52084710409_8f98a986ee_c_640_480_nofilter.jpg"/>
+          <p className="description">apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple </p>
+          <div className="stats">
+            <p>STATS</p>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Joined</td>
+                        <td>
+                          11:49 p.m.
+                          Monday, October 3, 2022 (EDT)
+                          Time in Toronto, ON
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Events attended</td>
+                        <td>with two columns</td>
+                    </tr>
+                    <tr>
+                        <td>Events hosted</td>
+                        <td>with two columns</td>
+                    </tr>
+                </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+        <div className="container-header">
+          <h1>EVENTS</h1>
+        </div>
+      <div className="event-container">
+        <Event />
+        <Event />
+        <Event />
+      </div>
+    </>
+  );
+}
