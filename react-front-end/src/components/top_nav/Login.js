9225559cb088b5ccf6e0ea81f5afb26f0c05f928
Donny Zhao <donny.zh@hotmail.com>
2022-10-04 13:32:16 -0400

    updating login component

--- react-front-end/src/components/top_nav/Login.js
+++ react-front-end/src/components/top_nav/Login.js
@@ -1,24 +1,40 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState, useEffect } from 'react';
+
 export default function TopNav() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [username, password]);
+
   return (
-    <Form>
-      <Form.Group className="form-floating mb-3" controlId="formBasicEmail">
-        <Form.Control type="email" placeholder="Enter email" id="floatingInput" />
-        <Form.Label>Email address</Form.Label>
-      </Form.Group>
+    <form>
+      <div class="form-floating mb-3">
+        <input 
+        type="text" 
+        id="floatingInput" 
+        placeholder="Username" 
+        autoComplete='off' 
+        onChange={(e) => setUsername(e.target.value)} 
+        value={username} 
+        required 
+        class="form-control"
+        
+        />
+        <label for='floatingInput'>Username:</label>
+      </div>
 
-      <Form.Group className="form-floating mb-3" controlId="formBasicPassword">
-        <Form.Control type="password" placeholder="Password" id="floatingInput" />
-        <Form.Label>Password</Form.Label>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+      <div class="form-floating mb-3">
+        <input type="password" id="floatingPassword" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} required class="form-control" />
+        <label htmlFor='floatingPassword'>Password:</label>
+      </div>
+      <button class="btn btn-primary" type="submit">
+        Login
+      </button>
+    </form>
   )
 };
