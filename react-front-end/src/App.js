8bb8a5662446062971b70ca6aea733beb676fa60
Daniel Suen <danielsuen2007@yahoo.ca>
2022-10-04 23:14:08 -0400

    added mode change

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
-import axios from 'axios';
+import About from './components/about/about';
+import Main from './components/main/Main';
 import Profile from './components/profile/profile';
+import Logged from './components/main_logged/main-logged';
+import React, { useState } from 'react'; 
+
+const ABOUT = "About";
+const USER = "User";
+const MAIN = "Main";
+const MAINLOGGED = "Mainlogged";
+const REGISTER = "Register";
+const EVENT = "Event";
+const CREATE = "Create";
 
 
-class App extends Component {
-  render() {
+export default function App() { 
+    const [mode, setmode] = useState(ABOUT); 
     return (
       <div className="App">
-        <Profile/>   
+        {mode === ABOUT && <About/>}
+        {mode === USER && <Profile/>}
+        {mode === MAIN && <Main/>}
+        {mode === MAINLOGGED && <Logged/>}
+        {/* {mode === EVENT && <Event/>} */}
+        {/* {mode === REGISTER && <Register/>} */}
+        {/* {mode === CREATE && <Create/>} */}
       </div>
     );
-  }
 }
 
-export default App;
