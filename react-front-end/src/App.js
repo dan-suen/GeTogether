65bad003c59782ec49bb44cbdee9394706f16618
Eds-Dbug <edsDbug@gmail.com>
2022-10-05 04:45:38 +0000

    refactor Events List as reusable component.

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import axios from 'axios';
-//import Main from './components/main/Main';
-import EventPage from 'components/event/EventPage';
+import Main from './components/main/Main';
+//import EventPage from 'components/event/EventPage';
+//import User from 'components/user/User';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <EventPage></EventPage>
+        <Main></Main>
       </div>
     );
   }
---
+++ react-front-end/src/components/event/EventsList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './eventsList.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
+const EventsList = (props) => {
+  return (
+    <>
+       <div className='header'>
+          <h5>Your Events</h5>
+          <button className='btn btn-primary'><FontAwesomeIcon icon={faPlus} /></button>
+        </div>
+        
+        <ul className="list-group list-group-flush">
+          {props.children}
+        </ul>
+    </>
+  );
+};
+
+export default EventsList;
