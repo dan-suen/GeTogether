3730f44402b582a5347fac60860351446b6594bc
Eds-Dbug <edsDbug@gmail.com>
2022-10-05 02:25:57 +0000

    complete views main

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import axios from 'axios';
-import Main from './components/main/Main';
+//import Main from './components/main/Main';
+import EventPage from 'components/event/EventPage';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Main></Main>     
+        <EventPage></EventPage>
       </div>
     );
   }
